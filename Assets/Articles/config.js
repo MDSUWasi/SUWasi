2b8075a8bd8883eec3b5230793d9f9704934e596
Wasi <shafiun.wasi@outlook.com>
2026-07-02 15:22:18 +0600

    Added Article page

---
+++ Assets/Articles/config.js
@@ -0,0 +1,11 @@
+const articlesDB = [
+    {
+        id: "No-Cloning Theorem",
+        title: "The Universe's Copy-Paste Error: Understanding No-Cloning.",
+        category: "QUANTUM PHYSICS ",
+        date: "May 12, 2026",
+        description: "No-Cloning theorem is the natures own copy protection. Quantum states cannot be copied.",
+        file: "articles/No-Cloning Theorem.html" 
+    },
+
+];
